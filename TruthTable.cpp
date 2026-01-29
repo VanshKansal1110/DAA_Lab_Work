626b15fa6483bcc0ab75f7a88a70b21bb5cae91d
Vansh Kansal <vanshkansal1110@gmail.com>
2026-01-29 18:26:29 +0530

    sum_of_n_numbers

---
+++ TruthTable.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+// # include <string>
+using namespace std;
+
+void TT(int num, string prev)
+{
+    if(prev.size()==num)
+    {
+        for(int i=0; i<num; i++)
+        {
+            if(prev[i]=='1')
+            cout<<"True\t";
+            else
+            cout<<"False\t";
+        }
+        cout<<endl;
+        return;
+    }
+    string temp= prev+ "1";
+    TT(num, temp);
+    temp= prev+ "0";
+    TT(num, temp);
+}
+
+int main() {
+    // Number of Statements to check the permutations for
+    int num;
+    cout<<"Enter the Number of the Statements:-\t";
+    cin>>num;
+    for(int i=0; i<num; i++){
+        cout<<"S"<<i+1<<"\t";
+    }
+    cout<<endl;
+    TT(num, "");
+}
